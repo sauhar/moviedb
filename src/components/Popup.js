a9ad2d5deb306c1f7c7d0e5f2a014966379f90fd
Sauhar khanal <sauhark@gmail.com>
2020-09-21 15:08:35 +0545

    commit

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 function Popup({ selected, closePopup }) {
 	return (
 		<section className="popup">
@@ -8,9 +9,18 @@ function Popup({ selected, closePopup }) {
 				<p className="rating">Rating: {selected.imdbRating}</p>
 				<div className="plot">
 					<img src={selected.Poster} alt="" />
-					<p>{selected.Plot}</p>
+					<p>{selected.Plot} <br></br> {selected.Awards} <br></br>BoxOffice Income {selected.BoxOffice} </p>
+					
+					
+								
 				</div>
-				<button className="close" onClick={closePopup}>Close</button>
+				<div>
+				<button className="close"  onClick={closePopup}>Close</button>
+				</div>
+				<br></br>
+				<div>
+				<a href={`https://www.imdb.com/title/${selected.imdbID}`} target="_blank" rel="noopener noreferrer" class="btn btn-primary"><button  className="close">View IMDB</button></a>
+				</div>
 			</div>
 		</section>
 	)

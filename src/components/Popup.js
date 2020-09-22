8fde9cd85b82db5097dee831b3bd7c14ea190e2b
Sauhar khanal <sauhark@gmail.com>
2020-09-22 08:50:50 +0545

    commit

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -9,18 +9,21 @@ function Popup({ selected, closePopup }) {
 				<p className="rating">Rating: {selected.imdbRating}</p>
 				<div className="plot">
 					<img src={selected.Poster} alt="" />
-					<p>{selected.Plot} <br></br> {selected.Awards} <br></br>BoxOffice Income {selected.BoxOffice} </p>
-					
-					
-								
+					<ul >
+					<li ><strong style={{color:'#D2691E'}}>Genre: </strong> {selected.Genre}</li>
+					<li ><strong style={{color:'#D2691E'}}>BoxOffice: </strong>{selected.BoxOffice}</li>
+					<li ><strong style={{color:'#D2691E'}}>Awards: </strong>{selected.Awards}</li>
+					</ul>
+									
 				</div>
-				<div>
-				<button className="close"  onClick={closePopup}>Close</button>
-				</div>
-				<br></br>
 				<div>
 				<a href={`https://www.imdb.com/title/${selected.imdbID}`} target="_blank" rel="noopener noreferrer" class="btn btn-primary"><button  className="close">View IMDB</button></a>
 				</div>
+				<br></br>
+				<div>
+				<button className="close"  onClick={closePopup}>Go Back To Search</button>
+				</div>
+				
 			</div>
 		</section>
 	)

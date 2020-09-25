f4025b735af7a9d128dff357cc2b8becb9d776a1
Sauhar khanal <sauhark@gmail.com>
2020-09-25 17:13:52 +0545

    commit

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -5,7 +5,7 @@ function Popup({ selected, closePopup }) {
 	return (
 		<section className="popup">
 			<div className="content">
-				<h2>{ selected.Title } <span>({ selected.Year })</span></h2>
+				<h2>{ selected.Title } <span>({ selected.Released })</span></h2>
 				<p className="rating">Rating: {selected.imdbRating}</p>
 				<div className="plot">
 					<img src={selected.Poster} alt="" />
